2468dafdb7d0e465a9f21bc2bf18e3894e82625c
Evolve <Artevolve@yahoo.com>
2021-11-24 06:31:27 +0100

    feat: /account/:address route

--- apis/account.js
+++ apis/account.js
@@ -26,4 +26,35 @@ router.get("/getaccountinfo", auth, async (req, res) => {
 	}
 });
 
+router.get("/nonce/:address", auth, async (req, res) => {
+	try {
+		let address = toLowerCase(req.params.address);
+		if (!ethers.utils.isAddress(address))
+			return res.json({
+				status: "failed",
+				data: "invalid erc20 address",
+			});
+		let account = await Account.findOne({ address: address });
+		if (account) {
+			return res.json({
+				status: "success",
+				data: account.nonce,
+			});
+		} else {
+			let _account = new Account();
+			_account.address = address;
+			_account.nonce = Math.floor(Math.random() * 9999999);
+			let __account = await _account.save();
+			return res.json({
+				status: "success",
+				data: __account.nonce,
+			});
+		}
+	} catch (error) {
+		return res.json({
+			status: "failed",
+		});
+	}
+});
+
 module.exports = router;
